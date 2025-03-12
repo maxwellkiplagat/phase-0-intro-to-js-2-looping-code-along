1693345f2580a08db4159f7e37e9e5a75574e45d
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-03-12 10:53:14 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(letter, houston){
+    let myArray=[];
+    for (let i = 0; i < letter.length; i++) {
+        myArray.push(`Thank you, ${letter[i]}, for the wonderful ${houston} gift!`)
+    }
+    return myArray;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
